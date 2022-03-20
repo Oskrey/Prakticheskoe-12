b11d089196545cbdaf8835f7b3c57e73366980b5
Никита Педич <pedichn@gmail.com>
2022-03-20 14:20:46 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практическое_задание_12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime birthday;
+            if (DateTime.TryParse(maskedTextBox1.Text, out birthday) == false)
+            {
+                MessageBox.Show("Ошибка в дате");
+            }
+            else
+            {
+                int age = DateTime.Today.Year - birthday.Year;
+                if (birthday.Date > DateTime.Today.AddYears(-age)) age--;
+                label1.Text = age.ToString();
+                label1.Text = birthday.CompareTo(DateTime.Parse("01.01.1900")).ToString();
+                if (age == 0)
+                {
+                    System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                }
+                if (birthday.CompareTo(DateTime.Parse("01.01.1900")) < 0)
+                {
+                    System.Diagnostics.Process.Start("https://ru.wikipedia.org/wiki/Вторая_мировая_война");
+                }
+                else if (age >= 18)
+                {
+                    System.Diagnostics.Process.Start("https://www.kinopoisk.ru");
+                }
+                else if (age <= 18)
+                {
+                    System.Diagnostics.Process.Start("https://pl.spb.ru");
+                }
+                
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Практическое задание 12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Практическое задание 12")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5817c995-2d8d-4769-b47a-9622f7278fbd")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
